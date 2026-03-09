0eba6b5db835d2c0ab8ca4f2fa727c526fb9f6c2
Aaron Alpar <aaron.alpar@gmail.com>
2026-03-09 10:29:51 -0700

    test(mvregister): semilattice properties, delta propagation, edge cases

--- mvregister/mvregister_test.go
+++ mvregister/mvregister_test.go
@@ -1,6 +1,7 @@
 package mvregister
 
 import (
+	"sort"
 	"testing"
 
 	qt "github.com/frankban/quicktest"
@@ -121,5 +122,176 @@ func TestConflictResolution(t *testing.T) {
 			c.Assert(vals, qt.HasLen, 3)
 			c.Assert(vals, qt.ContentEquals, []string{"from-a", "from-b", "from-c"})
 		}
+	})
+}
+
+func TestMergeProperties(t *testing.T) {
+	c := qt.New(t)
+
+	c.Run("Idempotent", func(c *qt.C) {
+		a := New[string]("a")
+		a.Write("x")
+
+		snapshot := New[string]("a")
+		snapshot.Merge(a)
+
+		a.Merge(snapshot)
+		a.Merge(snapshot)
+
+		c.Assert(a.Values(), qt.HasLen, 1)
+		c.Assert(a.Values()[0], qt.Equals, "x")
+	})
+
+	c.Run("Commutative", func(c *qt.C) {
+		a := New[string]("a")
+		b := New[string]("b")
+		a.Write("va")
+		b.Write("vb")
+
+		ab := New[string]("x")
+		ab.Merge(a)
+		ab.Merge(b)
+
+		ba := New[string]("x")
+		ba.Merge(b)
+		ba.Merge(a)
+
+		valsAB := ab.Values()
+		valsBA := ba.Values()
+		sort.Strings(valsAB)
+		sort.Strings(valsBA)
+		c.Assert(valsAB, qt.DeepEquals, valsBA)
+	})
+
+	c.Run("Associative", func(c *qt.C) {
+		a := New[string]("a")
+		b := New[string]("b")
+		x := New[string]("c")
+
+		a.Write("va")
+		b.Write("vb")
+		x.Write("vc")
+
+		// (a ⊔ b) ⊔ c
+		ab := New[string]("ab")
+		ab.Merge(a)
+		ab.Merge(b)
+		abc := New[string]("abc")
+		abc.Merge(ab)
+		abc.Merge(x)
+
+		// a ⊔ (b ⊔ c)
+		bc := New[string]("bc")
+		bc.Merge(b)
+		bc.Merge(x)
+		abc2 := New[string]("abc2")
+		abc2.Merge(a)
+		abc2.Merge(bc)
+
+		vals1 := abc.Values()
+		vals2 := abc2.Values()
+		sort.Strings(vals1)
+		sort.Strings(vals2)
+		c.Assert(vals1, qt.DeepEquals, vals2)
+	})
+}
+
+func TestDeltaPropagation(t *testing.T) {
+	c := qt.New(t)
+
+	c.Run("IncrementalEqualsFullMerge", func(c *qt.C) {
+		a := New[string]("a")
+		d1 := a.Write("first")
+		d2 := a.Write("second")
+
+		inc := New[string]("b")
+		inc.Merge(d1)
+		inc.Merge(d2)
+
+		full := New[string]("b")
+		full.Merge(a)
+
+		incVals := inc.Values()
+		fullVals := full.Values()
+		sort.Strings(incVals)
+		sort.Strings(fullVals)
+		c.Assert(incVals, qt.DeepEquals, fullVals)
+	})
+
+	c.Run("OverwriteDeltaSupersedes", func(c *qt.C) {
+		a := New[string]("a")
+		b := New[string]("b")
+
+		d1 := a.Write("first")
+		b.Merge(d1)
+
+		d2 := a.Write("second")
+		b.Merge(d2)
+
+		vals := b.Values()
+		c.Assert(vals, qt.HasLen, 1)
+		c.Assert(vals[0], qt.Equals, "second")
+	})
+
+	c.Run("DeltaDeltaMerge", func(c *qt.C) {
+		a := New[string]("a")
+		d1 := a.Write("first")
+		d2 := a.Write("second")
+
+		// Combine deltas, then apply.
+		d1.Merge(d2)
+
+		b := New[string]("b")
+		b.Merge(d1)
+
+		vals := b.Values()
+		c.Assert(vals, qt.HasLen, 1)
+		c.Assert(vals[0], qt.Equals, "second")
+	})
+
+	c.Run("ConcurrentWriteThenOverwrite", func(c *qt.C) {
+		a := New[string]("a")
+		b := New[string]("b")
+
+		da := a.Write("from-a")
+		db := b.Write("from-b")
+
+		a.Merge(db)
+		b.Merge(da)
+
+		d3 := a.Write("from-a-again")
+		b.Merge(d3)
+
+		vals := b.Values()
+		c.Assert(vals, qt.HasLen, 1)
+		c.Assert(vals[0], qt.Equals, "from-a-again")
+	})
+}
+
+func TestMergeWithEmpty(t *testing.T) {
+	c := qt.New(t)
+
+	c.Run("IntoEmpty", func(c *qt.C) {
+		a := New[string]("a")
+		a.Write("hello")
+
+		b := New[string]("b")
+		b.Merge(a)
+
+		vals := b.Values()
+		c.Assert(vals, qt.HasLen, 1)
+		c.Assert(vals[0], qt.Equals, "hello")
+	})
+
+	c.Run("EmptyIntoSet", func(c *qt.C) {
+		a := New[string]("a")
+		a.Write("hello")
+
+		empty := New[string]("b")
+		a.Merge(empty)
+
+		vals := a.Values()
+		c.Assert(vals, qt.HasLen, 1)
+		c.Assert(vals[0], qt.Equals, "hello")
 	})
 }
